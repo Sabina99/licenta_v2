3bbe5bbbe21719b66f04cc53eeab79de0fec229b
Sabina99 <sabina.apostol1910@gmail.com>
2022-06-01 14:29:29 +0300

    create relationships between models

--- app/Models/Actor.php
+++ app/Models/Actor.php
@@ -10,4 +10,9 @@ class Actor extends Model
     use HasFactory;
 
     protected $fillable = ['name', 'image', 'description', 'date_of_birth', 'external_id'];
+
+    public function movies(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
+    {
+        return $this->belongsToMany(Movie::class, 'movie_actors', 'actor_id','movie_id');
+    }
 }
--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -11,4 +11,8 @@ class Movie extends Model
 
     protected $fillable = ['title', 'image', 'directors', 'plot', 'imdb_rating', 'genres', 'external_id', 'year'];
 
+    public function actors(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
+    {
+        return $this->belongsToMany(Actor::class, 'movie_actors', 'movie_id','actor_id');
+    }
 }
